0052f5766234376ae61fda0ea339dbec47e4f2a7
Muskan <8922mt@gmail.com>
2024-03-31 12:37:23 +0545

    overall changes

--- hhdmoto/assets/js/active.js
+++ hhdmoto/assets/js/active.js
@@ -600,5 +600,38 @@
         sortableMasonry();
         isotopeBlock();
     });
-
+T
 })(window.jQuery);
+
+
+$(document).ready(function(){
+	 
+    //Show carousel-control
+    
+    $("#myCarousel").mouseover(function(){
+        $("#myCarousel .carousel-control").show();
+    });
+
+    $("#myCarousel").mouseleave(function(){
+        $("#myCarousel .carousel-control").hide();
+    });
+    
+    //Active thumbnail
+    
+    $("#thumbCarousel .thumb").on("click", function(){
+        $(this).addClass("active");
+        $(this).siblings().removeClass("active");
+    
+    });
+    
+    //When the carousel slides, auto update
+    
+    $('#myCarousel').on('slid.bs.carousel', function(){
+       var index = $('.carousel-inner .item.active').index();
+       //console.log(index);
+       var thumbnailActive = $('#thumbCarousel .thumb[data-slide-to="'+index+'"]');
+       thumbnailActive.addClass('active');
+       $(thumbnailActive).siblings().removeClass("active");
+       //console.log($(thumbnailActive).siblings()); 
+    });
+ });
